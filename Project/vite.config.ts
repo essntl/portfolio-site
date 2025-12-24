216f8219275e68f009757dac6fb503310d2c58d6
essntl <artemkotovno@proton.me>
2025-12-24 16:56:44 +0100

    Set up Code Server

--- Project/vite.config.ts
+++ Project/vite.config.ts
@@ -3,8 +3,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/', 
+  
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    hmr: {
+      clientPort: 443,
+    },
+    allowedHosts: true 
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
